c34a142183b15b69487cc288a51e26d31c968928
qff <qianfeng@ZMR.local>
2016-04-01 15:55:35 +0800

    创建一个A类

---
+++ GitDemo/GitDemo/PersonA.h
@@ -0,0 +1,13 @@
+//
+//  PersonA.h
+//  GitDemo
+//
+//  Created by qff on 16/4/1.
+//  Copyright © 2016年 祝孟然. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PersonA : NSObject
+
+@end
