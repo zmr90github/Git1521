e63f28bd3c96b6401e50e0c4f9788dcee01c9a39
qff <qianfeng@ZMR.local>
2016-04-01 16:05:51 +0800

    创建一个B类

---
+++ GitDemo/GitDemo/PersonB.h
@@ -0,0 +1,13 @@
+//
+//  PersonB.h
+//  GitDemo
+//
+//  Created by qff on 16/4/1.
+//  Copyright © 2016年 祝孟然. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PersonB : NSObject
+
+@end
---
+++ GitDemo/GitDemo/PersonB.m
@@ -0,0 +1,13 @@
+//
+//  PersonB.m
+//  GitDemo
+//
+//  Created by qff on 16/4/1.
+//  Copyright © 2016年 祝孟然. All rights reserved.
+//
+
+#import "PersonB.h"
+
+@implementation PersonB
+
+@end
